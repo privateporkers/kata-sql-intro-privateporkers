19d92a46da573caa84f0c0553682a05bbe495218
privateporkers <dccotton@firemail.cc>
2018-05-01 18:54:07 -0500

    implemented all GetProducts methods

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -6,13 +6,17 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            /*foreach (var prod in repo.GetProductWithReview())
+            /*var test = dapperOrProduct();
+            foreach (var prod in test.GetProductWithReview())
             {
                 Console.WriteLine($"Product Name: {prod.Name}  Reviews:  {prod.Comments}");
             }*/
 
             //DapperChoice();
             dapperCrudChoice(DapperChoice());
+
+
+
         }
 
         public static string DapperChoice()
@@ -32,42 +36,50 @@ namespace SqlIntro
             return answ;
         }
 
-        public static void dapperCrudChoice(string str)
+        public static IProductRepo dapperOrProduct()
         {
-            var prod = new Product();
+            IProductRepo choice;
             var connectionString = " Server=localhost;Database=adventureworks;Uid=root;Pwd=";
 
             Console.WriteLine("Do you want to use Dapper? (y or n)");
             var dap = Console.ReadLine();
 
-            IProductRepo repo;
 
             if(dap == "y")
             {
-                repo = new ProductRepository(connectionString);
+                choice = new DapperProductRepository(connectionString);
             }
             else
-            { 
-            repo = new DapperProductRepository(connectionString);
+            {
+                choice = new ProductRepository(connectionString);
             }
+
+            return choice;
+        }
+
+        public static void dapperCrudChoice(string str)
+        {
+            var produ = new Product();
+
+            var repo = dapperOrProduct();
 
             if(str == "u")
             {
                 Console.WriteLine("What ID do you want to specify?");
-                prod.Id = Convert.ToInt32( Console.ReadLine() );
+                produ.Id = Convert.ToInt32( Console.ReadLine() );
 
                 Console.WriteLine("What would you like to rename this product?");
-                prod.Name = Console.ReadLine();
+                produ.Name = Console.ReadLine();
 
-                repo.UpdateProduct(prod);
+                repo.UpdateProduct(produ);
             }
 
             if(str == "c")
             {
                 Console.WriteLine("What is your product called?");
-                prod.Name = Console.ReadLine();
+                produ.Name = Console.ReadLine();
 
-                repo.InsertProduct(prod);
+                repo.InsertProduct(produ);
             }
 
             if(str == "d")
@@ -80,7 +92,40 @@ namespace SqlIntro
 
             if(str == "a")
             {
-                repo.GetProducts();
+                Console.WriteLine("Would you like a full list(a), an inner list with review(b), or reviews and all products(c)");
+                string getProductsAnsw = " ";
+
+                while( !(getProductsAnsw == "a" || getProductsAnsw == "b" || getProductsAnsw == "c"))
+                {
+                    getProductsAnsw = Console.ReadLine();
+                    if(getProductsAnsw == "a" || getProductsAnsw == "b" || getProductsAnsw == "c")
+                    {
+                        break;
+                    }
+                    Console.WriteLine("That is not the correct input, please choose again");
+                }
+
+                switch(getProductsAnsw)
+                {
+                    case "a":
+                        foreach (var prod in repo.GetProducts())
+                        {
+                            Console.WriteLine($"Product Name: {prod.Name}");
+                        }
+                        break;
+                    case "b":
+                        foreach (var prod in repo.GetProductWithReview())
+                        {
+                            Console.WriteLine($"Product Name: {prod.Name}  Reviews:  {prod.Comments}");
+                        }
+                        break;
+                    case "c":
+                        foreach (var prod in repo.GetProductAndReview())
+                        {
+                            Console.WriteLine($"Product Name: {prod.Name}  Reviews:  {prod.Comments}");
+                        }
+                        break;
+                }
             }
         }
     }
